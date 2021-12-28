5520d35627c23757eaceb7b139fdeb442fa6d798
patryk_pa <patryk.majkut@profitaccumulator.co.uk>
2021-12-28 13:19:20 +0000

    vitex models update

--- epic_data_center/urls.py
+++ epic_data_center/urls.py
@@ -9,7 +9,7 @@ from vitex_feed import views as vi_view
 
 router = routers.DefaultRouter()
 router.register(r'block', ex_view.BlockView, 'block')
-router.register(r'vitex', vi_view.VitexUpdateView, 'vitex')
+router.register(r'vitex', vi_view.VitexHoldersUpdateView, 'vitex')
 
 
 urlpatterns = [
