abfed0db2b3d72d1af1fcac8b1fcaa0c04ad49b2
patryk_pa <patryk.majkut@profitaccumulator.co.uk>
2021-12-28 22:46:06 +0000

    new database endpoint (vitexUpdate)

--- vitex_feed/serializers.py
+++ vitex_feed/serializers.py
@@ -5,10 +5,10 @@ from .models import *
 class VitexUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = VitexUpdate
-        fields = ('__all__', )
+        fields = ('__all__')
 
 
 class VitexHoldersUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = VitexHoldersUpdate
-        fields = ('__all__', )
+        fields = ('__all__')
