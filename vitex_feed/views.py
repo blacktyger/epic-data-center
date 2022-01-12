4dbce62c1ebbe30ba96034e60877bea1a0ebea1f
blacktyger <blacktyg3r@gmail.com>
2022-01-12 13:22:31 +0000

    updated vitex model and api logic

--- vitex_feed/views.py
+++ vitex_feed/views.py
@@ -3,11 +3,25 @@ from rest_framework import viewsets
 from .serializers import *
 from .models import *
 
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from rest_framework import mixins
+from rest_framework import generics
 
-class UpdateView(viewsets.ModelViewSet):
+
+class UpdateView(mixins.ListModelMixin,
+                 mixins.CreateModelMixin,
+                 generics.GenericAPIView):
     """Endpoint to get the latest Vitex Exchange trading data for EPIC-001_BTC-000 pair"""
-    serializer_class = UpdateSerializer
+
     queryset = Update.objects.all()
+    serializer_class = UpdateSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        print(request, *args, **kwargs)
 
 
 class HistoryView(viewsets.ModelViewSet):
