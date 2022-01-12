582a63df02ce93ca9f3c89ac9265e35778c89aa9
blacktyger <blacktyg3r@gmail.com>
2022-01-12 12:38:15 +0000

    updated vitex model and api logic

--- epic_data_center/urls.py
+++ epic_data_center/urls.py
@@ -9,7 +9,7 @@ from vitex_feed import views as vitex_views
 router = routers.DefaultRouter()
 router.register(r'vitex/update', vitex_views.UpdateView, 'vitex-update')
 router.register(r'vitex/history', vitex_views.HistoryView, 'vitex-history')
-router.register(r'vitex/holders', vitex_views.HolderseView, 'vitex-holders')
+router.register(r'vitex/holders', vitex_views.HoldersView, 'vitex-holders')
 router.register(r'explorer/blocks', explorer_views.BlockView, 'explorer-blocks')
 
 
