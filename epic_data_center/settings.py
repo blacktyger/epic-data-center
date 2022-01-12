fd8f9351ae7c9eca547524b6fd09d6784b604b8b
blacktyger <blacktyg3r@gmail.com>
2022-01-12 16:53:10 +0000

    fixing

--- epic_data_center/settings.py
+++ epic_data_center/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'vitex_feed.apps.VitexFeedConfig',
 
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
     ]
 
@@ -58,6 +59,9 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'epic_data_center.urls'
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
     }
