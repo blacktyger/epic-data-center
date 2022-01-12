2532e7af7679a41bbc370487e0523317969120ba
blacktyger <blacktyg3r@gmail.com>
2022-01-12 12:39:50 +0000

    updated vitex model and api logic

--- vitex_feed/serializers.py
+++ vitex_feed/serializers.py
@@ -5,16 +5,16 @@ from .models import *
 class UpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Update
-        fields = ('__all__', )
+        fields = ('__all__')
 
 
 class HistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = History
-        fields = ('__all__', )
+        fields = ('__all__')
 
 
 class HoldersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Holders
-        fields = ('__all__', )
+        fields = ('__all__')
